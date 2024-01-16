cbb2ea1d2d9e2aec93e40968bfa8466a7d02c5ca
roberto <roberto.hernandez@one-beyond.com>
2024-01-16 21:31:55 +0100

    feat: add strapi and payloadCms page

---
+++ next-app/app/strapi/page.tsx
@@ -0,0 +1,9 @@
+const StrapiPage = () => {
+    return (
+        <div className="flex items-center justify-center max-h-full h-screen">
+            <h1> Strapi </h1>
+        </div>
+    )
+}
+
+export default StrapiPage
