08353f6196846b45503f0829f13c475ed5127cfc
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2024-01-23 10:06:07 +0100

    feat: one single instance of strapi and connect to localstack

---
+++ strapi/config/plugins.js
@@ -0,0 +1,24 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        endpoint: 'http://s3.localhost.localstack.cloud:4566',
+        params: {
+          ACL: env('AWS_ACL', 'public-read'),
+          signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
+          Bucket: env('AWS_BUCKET'),
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+
+});
