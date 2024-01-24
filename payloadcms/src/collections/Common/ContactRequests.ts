92abd3de3796830564ec61c764b4abebcf1fd09a
pabloelisseo <pabloelisseo@gmail.com>
2024-01-24 10:27:26 +0100

    feat(payload): refactor and add form collections

---
+++ payloadcms/src/collections/Common/ContactRequests.ts
@@ -0,0 +1,25 @@
+import { CollectionConfig } from "payload/types";
+import { isAdmin } from "../../access/isAdmin";
+
+const ContactRequests: CollectionConfig = {
+  slug: "contact-requests",
+  access: {
+    // Anyone can create, even unauthenticated
+    create: () => true,
+    // No one can update, ever
+    update: () => false,
+    // Only admins can read
+    read: isAdmin,
+    // No one can delete, ever
+    delete: () => false,
+  },
+  fields: [
+    {
+      name: "message",
+      type: "textarea",
+      required: true,
+    },
+  ],
+};
+
+export default ContactRequests;
---
+++ payloadcms/src/collections/Common/Forms/CallForActions.ts
@@ -0,0 +1,38 @@
+import { CollectionConfig } from "payload/types";
+
+const CallForActions: CollectionConfig = {
+  slug: "form-call-for-actions",
+  labels: {
+    singular: "Call For Action",
+    plural: "Call For Actions",
+  },
+  admin: {
+    useAsTitle: "name",
+  },
+  versions: {
+    drafts: true,
+  },
+  access: {},
+  fields: [
+    {
+      name: "name",
+      label: "Name",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "caption",
+      label: "Caption",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "type",
+      label: "Type",
+      type: "select",
+      options: [{ label: "Submit", value: "submit" }],
+      required: true,
+    },
+  ],
+};
+export default CallForActions;
