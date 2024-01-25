e5d0721111238d0c9b1881d32ce6a13ace7e2161
roberto <roberto.hernandez@one-beyond.com>
2024-01-25 12:52:33 +0100

    styles: improve strapi form

--- next-app/app/strapi/page.tsx
+++ next-app/app/strapi/page.tsx
@@ -1,18 +1,21 @@
 "use client"
-import {getForm} from "@/app/actions/getForm";
+import {getPageData} from "@/app/actions/getPageData";
+import {NextPage} from "next";
 // @ts-ignore
-
 import {FormBuilder} from "@guidesmiths/react-form-builder"
+import Header from "@/src/components/Header";
+import React from "react";
 
-const StrapiPage = async () => {
-    const {form} = await getForm()
-    return (
-        <div className="flex items-center justify-center max-h-full h-screen">
-            <div className="flex">
-                {form && <FormBuilder form={form} onSubmit={() => console.log("sent")}/>}
-            </div>
-        </div>
-    )
+const StrapiPage: NextPage = async () => {
+  const {form, backgroundImage, title} = await getPageData()
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <Header img={backgroundImage} title={title}/>
+      <div className="flex flex-col w-1/2 mx-3 ">
+        {form && <FormBuilder form={form} onSubmit={() => console.log("sent")}/>}
+      </div>
+    </div>
+  )
 }
 
 
